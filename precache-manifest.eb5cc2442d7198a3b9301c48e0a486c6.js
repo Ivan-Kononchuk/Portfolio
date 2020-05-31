aa6895b22cff6cf88e2fd013ed95673577cc12e1
noname344 <myyzrik@gmail.com>
2020-05-31 14:32:51 +0300

    Updates

---
+++ precache-manifest.eb5cc2442d7198a3b9301c48e0a486c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecce57dc2d3d42ea0fbac169fb583a5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "27e587ca6eee822747e7",
+    "url": "/Portfolio/static/css/main.e26cfeb9.chunk.css"
+  },
+  {
+    "revision": "599369be5668e4aaf754",
+    "url": "/Portfolio/static/js/2.6639bea2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.6639bea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e587ca6eee822747e7",
+    "url": "/Portfolio/static/js/main.34789c79.chunk.js"
+  },
+  {
+    "revision": "0f1f50cf40f7c69d97ab",
+    "url": "/Portfolio/static/js/runtime-main.2c6fb4ab.js"
+  },
+  {
+    "revision": "db592233a5bd1ec035ec3581a17d7271",
+    "url": "/Portfolio/static/media/bluredImg.db592233.png"
+  },
+  {
+    "revision": "dc322d82c79ecaba2df922a8feccb660",
+    "url": "/Portfolio/static/media/gitIcon.dc322d82.png"
+  }
+]);
