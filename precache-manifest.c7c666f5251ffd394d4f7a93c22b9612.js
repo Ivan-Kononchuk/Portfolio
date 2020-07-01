db488cfd2ead086081a5a076cb932d4360a8b47d
Ivan-Kononchuk <zoibertonfd@mail.ru>
2020-07-01 15:08:13 +0300

    Updates

---
+++ precache-manifest.c7c666f5251ffd394d4f7a93c22b9612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f82cae408964fed3ecfa7db40d2614",
+    "url": "/Portfolio/Projects/index.html"
+  },
+  {
+    "revision": "5620faa31b423011ed15",
+    "url": "/Portfolio/Projects/static/css/main.2b7a5fd7.chunk.css"
+  },
+  {
+    "revision": "e72aa1a47d242403b490",
+    "url": "/Portfolio/Projects/static/js/2.42033944.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/Projects/static/js/2.42033944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5620faa31b423011ed15",
+    "url": "/Portfolio/Projects/static/js/main.16f6e317.chunk.js"
+  },
+  {
+    "revision": "bb20590280c53ad1671e",
+    "url": "/Portfolio/Projects/static/js/runtime-main.d2bd40a1.js"
+  },
+  {
+    "revision": "0b3f0989edfb8e40ff18bbbaa660b8a5",
+    "url": "/Portfolio/Projects/static/media/bluredImg.0b3f0989.jpg"
+  },
+  {
+    "revision": "f3db75059346b0fc4f44e1ab30974654",
+    "url": "/Portfolio/Projects/static/media/gitIcon.f3db7505.png"
+  }
+]);
